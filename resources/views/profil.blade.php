cd543625e912a4a2c877b2943284886bb98e023c
RifqiErawan <rfqrwn@gmail.com>
2020-07-20 20:07:58 +0700

    add konselor profile page

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -9,6 +9,45 @@
     <link rel="stylesheet" href="{{ asset('assets/admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/admin-lte/dist/css/adminlte.min.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <style>
+    * {
+    box-sizing: border-box;
+    }
+
+    /* Float four columns side by side */
+    .column {
+    float: left;
+    width: 25%;
+    padding: 0 10px;
+    }
+
+    /* Remove extra left and right margins, due to padding */
+    .row {margin: 0 -5px;}
+
+    /* Clear floats after the columns */
+    .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    }
+
+    /* Responsive columns */
+    @media screen and (max-width: 600px) {
+    .column {
+        width: 100%;
+        display: block;
+        margin-bottom: 20px;
+    }
+    }
+
+    /* Style the counter cards */
+    .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    padding: 16px;
+    text-align: center;
+    background-color: #f1f1f1;
+    }
+    </style>
 </head>
 <body class="hold-transition layout-top-nav">
     <div class="wrapper">
@@ -80,7 +119,7 @@
                 <div class="container">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">Beranda</h1>
+                            <h1 class="m-0 text-dark">Profil Tim Konselor</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
@@ -95,93 +134,22 @@
             <!-- Main content -->
             <div class="content">
                 <div class="container">
-
-                    <div class="row">
-
-                        <!-- Blog Entries Column -->
-                        <div class="col-md-8">
-                            <!-- Blog Post -->
-                            <div class="card mb-4">
-                                <img class="card-img-top" src="https://via.placeholder.com/728x180.png?text=Gambar+Posting+BKP" alt="Card image cap">
-                                <div class="card-body">
-                                    <h2 class="card-title"><b>Roadshow BKP bagi Mahasiswa Angkatan 2015</b></h2>
-                                    <p class="card-text">By Muhammad Saiful Islam / 12 Maret 2016</p>
-                                    <p class="card-text">Pada hari Rabu (2/3), UPT Bimbingan, Konseling, dan Pendampingan (BKP) Politeknik Negeri Bandung menyelenggarakan roadshow bagi mahasiswa JTK angkatan 2015. Kunjungan BKP ke JTK merupakan kunjungan pertama dari 14 kunjungan yang dijadwalkan bagi 14 jurusan yang ada di Polban.</p>
-                                    <p class="card-text">Roadshow diselenggarakan di Ruang Serbaguna, lantai 2 gedung JTK Polban pada pukul 15.30 WIB, dengan materi dari Muhammad Arman, S.T., S.Psi., M.T. selaku Ketua UPT BKP Polban. Pada kesempatan ini, Arman membahas mengenai permasalahan yang biasa dialami oleh mahasiswa tingkat satu, seperti kesulitan mengatur waktu belajar, kurangnya motivasi dan semangat belajar, salah cara belajar, sulit menyesuaikan diri, toleransi tinggal di asrama/tempat indekos, dan frustasi serta konflik pribadi.</p>
-                                    <p class="card-text">Ditanyai mengenai kegiatan tersebut, Novia Sukmasari, salah satu mahasiswa JTK mengaku mendapatkan pengetahuan baru serta sedikit-banyak termotivasi dari kegiatan tersebut. Terutama, karena permasalahan yang dibahas memang merupakan hal-hal yang sedang dialami oleh para mahasiswa tingkat 1, dan juga ada masalah-masalah yang tidak disadari sebelumnya.</p>
-                                    <p class="card-text">Selain itu, Nita Amelia, mahasiswa JTK lainnya juga mengungkapkan bahwa setelah pengumuman hasil semester pertama di JTK, banyak yang merasa kecewa dengan perolehan akademik masing-masing. Karenanya, roadshow yang diselenggarakan UPT BKP ini cukup bagus. Selain itu, penyampaian Arman sebagai pemateri — yang juga merupakan lulusan S-1 Psikologi Universitas Padjadjaran tahun 1997 — dinilai baik karena dapat “menggali” para mahasiswa yang ragu untuk mengungkapkan pikirannya. (MS)</p>
-                                    <p class="card-text">Foto utama: Muhammad Arman pada sesi tanya jawab setelah penyampaian materinya di Roadshow UPT BKP Polban. (Sumber: Departemen Komunikasi dan Informasi, Himpunan Mahasiswa Komputer)</p>
-                                </div>
-                                <div class="card-footer text-muted">
-                                    Posted on <?php echo Carbon\Carbon::now()->toDateTimeString(); ?> by
-                                    <a href="#">Admin</a>
-                                </div>
-                            </div>
-
-                            <!-- Pagination -->
-                            <ul class="pagination justify-content-center mb-4">
-                                <li class="page-item">
-                                    <a class="page-link" href="#">Lama</a>
-                                </li>
-                                <li class="page-item active">
-                                    <a class="page-link" href="#">1</a>
-                                </li>
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#">Baru</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-4">
-
-                            <!-- Search Widget -->
-                            <div class="card my-4">
-                                <h5 class="card-header">Cari</h5>
-                                <div class="card-body">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Cari ...">
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-secondary" type="button">Cari</button>
-                                        </span>
+                    <div class="container page-container">
+                    @foreach(($list_konselor) as $konselor)
+                        <!-- <div class="row"> -->
+                            <div class="column">
+                                <div class="card">
+                                    <div class="container">
+                                        <p><b>{{$konselor->nama}}</b></p>
+                                        <p class="title">{{$konselor->program_studi_id}}</p>
+                                        <p>{{$konselor->email}}</p>
+                                        <p><button class="button">Contact</button></p>
                                     </div>
                                 </div>
                             </div>
-
-                            <!-- Categories Widget -->
-                            <div class="card my-4">
-                                <h5 class="card-header">Label</h5>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <ul class="list-unstyled mb-0">
-                                                <li>
-                                                    <a href="#">Psychology</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Relationship</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Motivation</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card my-4">
-                                <h5 class="card-header">
-                                    Social Page
-                                </h5>
-                                <div class="fb-page" data-href="https://www.facebook.com/" data-tabs="timeline" data-width="900" data-height="600" data-small-header="false" data-adapt-container-width="true"
-                                  data-hide-cover="false" data-show-facepile="true">
-                                    <blockquote cite="https://www.facebook.com/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/">Facebook</a></blockquote>
-                                </div>
-                            </div>
-
-                        </div>
-
+                        <!-- </div> -->
+                    @endforeach
                     </div>
-                    <!-- /.row -->
-
                 </div>
             </div>
             <!-- /.content -->
